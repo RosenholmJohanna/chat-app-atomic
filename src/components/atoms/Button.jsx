fb6bc70547cd13a88e5d1a2352daef0e10f5130d
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-09-15 16:46:29 +0200

    components/files set up using atomic pattern

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #1a2f4b;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #17354d;
+  }
+`;
+
+export default Button;
---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  padding: 8px;
+  font-size: 16px;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+`;
+
+export default Input;
